f41fbb81adde8ee5a36563dcbfb8e66dbe6fd2ed
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-05-01 17:33:45 -0400

    Adding more basic examples

---
+++ src/random_operations.py
@@ -0,0 +1,74 @@
+# Array Operations
+
+def find_largest_smallest_item(input_array: list) -> tuple:
+    """
+    Function to find the largest and smallest items in an array
+    :param input_array: Input array
+    :return: Tuple of largest and smallest items
+    """
+
+    if len(input_array) == 0:
+        raise ValueError
+    # Set the initial values of largest and smallest to the first item in the array
+    largest = input_array[0]
+    smallest = input_array[0]
+
+    # Iterate through the array
+    for i in range(1, len(input_array)):
+        # If the current item is larger than the current value of largest, update largest
+        if input_array[i] > largest:
+            largest = input_array[i]
+        # If the current item is smaller than the current value of smallest, update smallest
+        if input_array[i] < smallest:
+            smallest = input_array[i]
+
+    return largest, smallest
+
+
+def sort_array(input_array: list, sort_key: str) -> list:
+    """
+    Function to sort an array
+    :param sort_key: Sort key
+    :param input_array: Input array
+    :return: Sorted array
+    """
+    if len(input_array) == 0:
+        raise ValueError
+    if sort_key not in input_array[0]:
+        raise KeyError
+    if not isinstance(input_array[0][sort_key], int):
+        raise TypeError
+    sorted_data = sorted(input_array, key=lambda x: x[sort_key], reverse=True)
+    return sorted_data
+
+
+# String Operations
+
+def reverse_string(input_string) -> str:
+    """
+    Function to reverse a string
+    :param input_string: Input string
+    :return: Reversed string
+    """
+    return input_string[::-1]
+
+
+def complex_string_operation(input_string: str) -> str:
+    """
+    Function to perform a complex string operation
+    :param input_string: Input string
+    :return: Transformed string
+    """
+    # Remove Whitespace
+    input_string = input_string.strip().replace(" ", "")
+
+    # Convert to Upper Case
+    input_string = input_string.upper()
+
+    # Remove vowels
+    vowels = ("A", "E", "I", "O", "U")
+    for x in input_string.upper():
+        if x in vowels:
+            input_string = input_string.replace(x, "")
+
+    return input_string
---
+++ tests/unit/test_random_operations.py
@@ -0,0 +1,73 @@
+import pytest
+from hypothesis import given, strategies as st, example
+from hypothesis import assume as hypothesis_assume
+import logging
+from src.random_operations import (
+    reverse_string,
+    find_largest_smallest_item,
+    complex_string_operation,
+    sort_array
+)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Example Based Unit Testing
+def test_find_largest_smallest_item():
+    assert find_largest_smallest_item([1, 2, 3]) == (3, 1)
+
+
+def test_reverse_string():
+    assert reverse_string("hello") == "olleh"
+
+
+def test_sort_array():
+    data = [
+        {"name": "Alice", "age": 25},
+        {"name": "Bob", "age": 30},
+        {"name": "Charlie", "age": 20},
+        {"name": "David", "age": 35},
+    ]
+    assert sort_array(data, "age") == [
+        {"name": "David", "age": 35},
+        {"name": "Bob", "age": 30},
+        {"name": "Alice", "age": 25},
+        {"name": "Charlie", "age": 20},
+    ]
+
+
+def test_complex_string_operation():
+    assert complex_string_operation("  Hello World  ") == "HLLWRLD"
+
+
+# Property Based Unit Testing
+@given(st.lists(st.integers(), min_size=1))
+def test_find_largest_smallest_item_hypothesis(input_list):
+    assert find_largest_smallest_item(input_list) == (max(input_list), min(input_list))
+
+
+@given(st.lists(
+    st.fixed_dictionaries({
+        "name": st.text(),
+        "age": st.integers()}
+    ),
+    ))
+def test_sort_array_hypothesis(input_list):
+    if len(input_list) == 0:
+        with pytest.raises(ValueError):
+            sort_array(input_list, "age")
+
+    hypothesis_assume(len(input_list) > 0)
+    assert sort_array(input_list, "age") == sorted(input_list, key=lambda x: x["age"], reverse=True)
+
+
+@given(st.text())
+def test_reverse_string_hypothesis(input_string):
+    assert reverse_string(input_string) == input_string[::-1]
+
+
+@given(st.text())
+def test_complex_string_operation_hypothesis(input_string):
+    assert complex_string_operation(input_string) == input_string.strip().replace(" ", "") \
+        .upper().replace("A", "").replace("E", "").replace("I", "").replace("O", "").replace("U", "")
